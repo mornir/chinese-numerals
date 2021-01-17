4871bfd5cfbf7bc8caec383b31a88058fdb8588f
mornir <jeromepott@hotmail.com>
2021-01-17 20:34:55 +0100

    Better looking select

--- src/components/FontSelect.vue
+++ src/components/FontSelect.vue
@@ -1,52 +1,57 @@
 <template>
-  <Listbox v-model="selectedFont">
-    <ListboxLabel class="block">Change font</ListboxLabel>
-    <ListboxButton
-      class="flex items-center justify-between w-full px-2 py-2 border-2 rounded-lg shadow-md border-primary focus:outline-none focus:bg-red-100"
-      >{{ selectedFont.pinyin }} ({{ selectedFont.hanzi }})
-      <svg
-        class="w-5 h-5 text-gray-700 stroke-current"
-        viewBox="0 0 20 20"
-        fill="none"
-      >
-        <path
-          d="M7 7l3-3 3 3m0 6l-3 3-3-3"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        /></svg
-    ></ListboxButton>
-    <transition
-      enter-active-class="transition duration-100 ease-out"
-      enter-from-class="transform scale-95 opacity-0"
-      enter-to-class="transform scale-100 opacity-100"
-      leave-active-class="transition duration-75 ease-out"
-      leave-from-class="transform scale-100 opacity-100"
-      leave-to-class="transform scale-95 opacity-0"
-    >
-      <ListboxOptions>
-        <!-- Use the `active` state to conditionally style the active option. -->
-        <!-- Use the `selected` state to conditionally style the selected option. -->
-        <ListboxOption
-          as="template"
-          v-slot="{ active, selected }"
-          v-for="font in fonts"
-          :key="font.id"
-          :value="font"
-          class="p-4"
+  <div class="font-medium font-body">
+    <Listbox v-model="selectedFont">
+      <ListboxLabel class="block pl-2">Change font</ListboxLabel>
+      <ListboxButton
+        class="flex items-center justify-between w-full px-2 py-2 mb-2 border-2 rounded-lg shadow-md border-primary focus:outline-none focus:bg-red-100"
+        >{{ selectedFont.pinyin }} ({{ selectedFont.hanzi }})
+        <svg
+          class="w-5 h-5 text-gray-700 stroke-current"
+          viewBox="0 0 20 20"
+          fill="none"
         >
-          <li
-            :class="{
-              'bg-red-200': active,
-            }"
+          <path
+            d="M7 7l3-3 3 3m0 6l-3 3-3-3"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          /></svg
+      ></ListboxButton>
+      <transition
+        enter-active-class="transition duration-100 ease-out"
+        enter-from-class="transform scale-95 opacity-0"
+        enter-to-class="transform scale-100 opacity-100"
+        leave-active-class="transition duration-75 ease-out"
+        leave-from-class="transform scale-100 opacity-100"
+        leave-to-class="transform scale-95 opacity-0"
+      >
+        <ListboxOptions
+          class="py-2 border-2 rounded-xl focus:bg-red-100 border-primary focus:outline-none"
+        >
+          <!-- Use the `active` state to conditionally style the active option. -->
+          <!-- Use the `selected` state to conditionally style the selected option. -->
+          <ListboxOption
+            as="template"
+            v-slot="{ active, selected }"
+            v-for="font in fonts"
+            :key="font.id"
+            :value="font"
+            class="p-2"
           >
-            <!--    <CheckmarkIcon v-show="selected" /> -->
-            {{ font.pinyin }}
-          </li>
-        </ListboxOption>
-      </ListboxOptions>
-    </transition>
-  </Listbox>
+            <li
+              :class="{
+                'bg-red-500 text-white': active,
+              }"
+              class="focus:outline-none"
+            >
+              <!--    <CheckmarkIcon v-show="selected" /> -->
+              {{ font.pinyin }} ({{ font.hanzi }})
+            </li>
+          </ListboxOption>
+        </ListboxOptions>
+      </transition>
+    </Listbox>
+  </div>
 </template>
 
 <script>
